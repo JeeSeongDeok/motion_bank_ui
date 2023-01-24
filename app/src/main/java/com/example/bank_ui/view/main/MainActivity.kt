765ae4653b87c1c9a60df90713719e053a77103e
JeeSeongDeok <bh939@naver.com>
2023-01-24 14:51:33 +0900

    메인화면 디자인 초안

---
+++ app/src/main/java/com/example/bank_ui/view/main/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.bank_ui.view.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.bank_ui.R
+
+/**
+ * 디자인
+ * https://dribbble.com/shots/16116630-Bank-mobile-app
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
